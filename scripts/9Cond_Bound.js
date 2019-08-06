78b38b007046e83f6eff4265a6640031ab63bd70
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2019-08-06 14:28:32 +0100

    INTERACTIVITY DONE

--- scripts/9Cond_Bound.js
+++ scripts/9Cond_Bound.js
@@ -78,6 +78,41 @@ function setupyReflectionEnvelopeData (xMin, xMax, plotStep, amplitude, omega, s
 
 }
 
+function dataMediumCompile (xMin, xMax, plotStep) {
+    let zSurface = [[10, 10.625, 12.5, 15.625, 20],
+    [5.625, 6.25, 8.125, 11.25, 15.625],
+    [2.5, 3.125, 5.0, 8.125, 12.5],
+    [0.625, 1.25, 3.125, 6.25, 10.625],
+    [0, 0.625, 2.5, 5.625, 10]];
+    let xSurface = [xMin, xMax/4, xMax/3, xMax/2, xMax/1];
+    let ySurface = [xMin, xMax/4, xMax/3, xMax/2, xMax/1];
+
+//    let dataMedium = {
+//                         x: xSurface,
+//                         y: ySurface,
+//                         z: zSurface,
+//                         type: 'contour',
+//                         opacity : 1,
+//                         showscale: true,
+//                         colorscale: 'Jet',
+//                     };
+//    return dataMedium;
+
+let data = {
+  z: [[10,10,10,10,10],
+     [10,10,10,10,10],
+     [10,10,10,10,10],
+     [10,10,10,10,10],
+     [10,10,10,10,10]],
+      x: [-9, -6, -5 , -3, -1],
+  y: [0, 1, 4, 5, 7],
+  type: 'contour',
+  opacity: 0.5,
+  colorscale: 'Jet',
+};
+return data;
+}
+
 function dataIncidentCompile(xLine, yLine) {
     let dataLine = {
                          x:xLine,
@@ -135,7 +170,7 @@ function dataReflectionEnvelopeCompile(xLine, yLine) {
                          mode: 'lines',
                          line: {
                                 dash: 'dashdot',
-                                color: 'rgb(186,85,211)',
+                                color: 'rgb(255,140,0)',
                                 width: 3
                               },
                          name: 'Reflected Amplitude',
@@ -160,11 +195,29 @@ function dataCombinedCompile(xLine, yLine) {
     return dataLine;
 };
 
-function dataPlot (xMin, xMax, t, c, plotStep) {
+function dataPlot (xMin, xMax, yMin, yMax, xEdge, t, c, plotStep) {
     let omega = parseFloat(document.getElementById('Slider_omega_9').value)* Math.pow(10,15);
     let sigma = parseFloat(document.getElementById('Slider_sigma_9').value)* Math.pow(10,5);
     let xLine = setupxData(xMin, xMax, plotStep);
     let amplitude = parseFloat(document.getElementById('Slider_E_9').value)* Math.pow(10,15);
+    let skinDepth = Math.sqrt( (2)/(4e-7 * Math.PI * sigma * omega));
+
+//    let dataMedium = dataMediumCompile(xMin, xMax, plotStep);
+    let opacityMedium = 0.3 + sigma/(200*Math.pow(10,5))*0.7;
+
+    let dataMedium = {
+          z: [[10,10,10,10,10],
+             [10,10,10,10,10],
+             [10,10,10,10,10],
+             [10,10,10,10,10],
+             [10,10,10,10,10]],
+              x: [0, xEdge/4, xEdge/3, xEdge/2,xEdge],
+          y: [yMin, yMin/Math.pow(10,10), 0, yMax/Math.pow(10,10), yMax],
+          type: 'contour',
+          opacity: opacityMedium,
+          colorscale: 'Jet',
+          showscale: false
+};
 
     let condition =  $("input[name = wave-switch]:checked").val();
 
@@ -173,9 +226,13 @@ function dataPlot (xMin, xMax, t, c, plotStep) {
     } else {
         document.getElementById("approximationMessage").style.display = "none"};
 
-    let radiationPressure = 8.85e-12 * amplitude**2
+    let radiationPressureFactor = 2 - Math.sqrt(8*8.85e-12*omega/sigma);
+    let radiationPressure = 0.5 * radiationPressureFactor * 8.85e-12 * amplitude**2;
 
-    $("#radiationPressure").text(`Radiation Pressure = ${Math.round(100*radiationPressure)/100}`);
+//    $("#radiationPressure").text("Radiation Pressure =" + Math.round(1000*(2 - Math.sqrt(8*8.85e-12*omega/sigma))) /1000 + "$F = ma$");
+    document.getElementById("radiationPressureFactor").innerHTML = Math.round(1000*(2 - Math.sqrt(8*8.85e-12*omega/sigma))) /1000;
+    document.getElementById("radiationPressure").innerHTML = Math.round(radiationPressure/Math.pow(10,16))/Math.pow(10,3);
+    document.getElementById("skinDepth").innerHTML = Math.round(skinDepth*Math.pow(10,11))/Math.pow(10,3);
 
     let yIncidentEnvelope = setupyIncidentEnvelopeData(xMin, xMax, plotStep, amplitude, omega, sigma);
     let dataIncidentEnvelope = dataIncidentEnvelopeCompile(xLine, yIncidentEnvelope);
@@ -186,15 +243,15 @@ function dataPlot (xMin, xMax, t, c, plotStep) {
     if (condition === "incident") {
         let yLine = setupyIncidentData(xMin, xMax, t, c, plotStep, amplitude, omega, sigma);
         let dataIncident = dataIncidentCompile (xLine, yLine);
-        return [dataIncident, dataIncidentEnvelope];
+        return [dataIncident, dataIncidentEnvelope, dataMedium];
     } else if (condition === "reflected") {
         let yLine = setupyReflectionData (xMin, xMax, t, c, plotStep, amplitude, omega, sigma);
         let dataReflection = dataReflectionCompile (xLine, yLine);
-        return [dataReflection, dataReflectionEnvelope];
+        return [dataReflection, dataIncidentEnvelope, dataReflectionEnvelope, dataMedium];
     } else if (condition === "resultant") {
         let yLine = setupyCombinedData (xMin, xMax, t, c, plotStep, amplitude, omega, sigma);
         let dataCombined = dataCombinedCompile (xLine, yLine);
-        return [dataCombined, dataIncidentEnvelope, dataReflectionEnvelope];
+        return [dataCombined, dataIncidentEnvelope, dataReflectionEnvelope, dataMedium];
     } else if (condition === "all"){
         let yIncidentLine = setupyIncidentData(xMin, xMax, t, c, plotStep, amplitude, omega, sigma);
         let dataIncident = dataIncidentCompile (xLine, yIncidentLine);
@@ -205,7 +262,7 @@ function dataPlot (xMin, xMax, t, c, plotStep) {
         let yCombinedLine = setupyCombinedData (xMin, xMax, t, c, plotStep, amplitude, omega, sigma);
         let dataCombined = dataCombinedCompile (xLine, yCombinedLine);
 
-        return [dataIncident, dataReflection, dataCombined];
+        return [dataIncident, dataReflection, dataCombined, dataMedium];
     };
 
 };
@@ -214,8 +271,8 @@ function plot(data, layout) {
     Plotly.react("Boundary_Plot_9", data, layout);
 }
 
-function compileAndPlot(xMin, xMax, t, c, plotStep, layout){
-    let data = dataPlot(xMin, xMax, t, c, plotStep);
+function compileAndPlot(xMin, xMax, yMin, yMax, xEdge, t, c, plotStep, layout){
+    let data = dataPlot(xMin, xMax, yMin, yMax, xEdge, t, c, plotStep);
     plot(data, layout);
 }
 
@@ -229,19 +286,22 @@ function main(){
     let isPlay = false;
     let t = 0;
 
+    const xEdge = xMax/4.5;
+    const yMin = xMin*Math.pow(10,21.5);
+    const yMax = xMax*Math.pow(10,21.5);
 
     const plotLayout = {
         title: "Radiation Pressure Exerted on a Conductor Boundary",
 //        showlegend: false,
         xaxis: {
             constrain: "domain",
-            range: [xMin, xMax/2.5],
+            range: [xMin, xEdge],
             title: "x",
             showticklabels: false
         },
         yaxis: {
 //            scaleanchor: "x",
-            range: [xMin*Math.pow(10,21.5), xMax*Math.pow(10,21.5)],
+            range: [yMin, yMax],
             showticklabels: false,
             title: "y"
         },
@@ -253,37 +313,37 @@ function main(){
     document.getElementById("approximationMessage").style.display = "none"
 
     function playLoop(){//adds time evolution
-    if (isPlay === false) {
-//        Plotly.animate("Boundary_Plot_9",
-//        {data: dataPlot(xMin, xMax, t, c, plotStep, initialAmplitude)},
-//        {
-//            fromcurrent: true,
-//            transition: {duration: 0},
-//            frame: {duration: 0, redraw: false,},
-//            //mode: "afterall"
-//            mode: "immediate"
-//        });
-        compileAndPlot(xMin, xMax, t, c, plotStep, plotLayout);
-    } else if (isPlay === true) {
-        t += 5e-18;
-//        Plotly.animate("Boundary_Plot_9",
-//            {data: dataPlot(xMin, xMax, t, c, plotStep, initialAmplitude)},
-//            {
-//                fromcurrent: true,
-//                transition: {duration: 0},
-//                frame: {duration: 0, redraw: false,},
-//                //mode: "afterall"
-//                mode: "immediate"
-//            });
-        compileAndPlot(xMin, xMax, t, c, plotStep, plotLayout);
-        requestAnimationFrame(playLoop);//loads next frame
-        }
+        if (isPlay === false) {
+    //        Plotly.animate("Boundary_Plot_9",
+    //        {data: dataPlot(xMin, xMax, t, c, plotStep, initialAmplitude)},
+    //        {
+    //            fromcurrent: true,
+    //            transition: {duration: 0},
+    //            frame: {duration: 0, redraw: false,},
+    //            //mode: "afterall"
+    //            mode: "immediate"
+    //        });
+            compileAndPlot(xMin, xMax, yMin, yMax, xEdge, t, c, plotStep, plotLayout);
+        } else if (isPlay === true) {
+            t += 1e-17;
+    //        Plotly.animate("Boundary_Plot_9",
+    //            {data: dataPlot(xMin, xMax, t, c, plotStep, initialAmplitude)},
+    //            {
+    //                fromcurrent: true,
+    //                transition: {duration: 0},
+    //                frame: {duration: 0, redraw: false,},
+    //                //mode: "afterall"
+    //                mode: "immediate"
+    //            });
+            compileAndPlot(xMin, xMax, yMin, yMax, xEdge, t, c, plotStep, plotLayout);
+            requestAnimationFrame(playLoop);//loads next frame
+            }
 
     };
 
 
 
-    compileAndPlot(xMin, xMax, t, c, plotStep, plotLayout);
+    compileAndPlot(xMin, xMax, yMin, yMax, xEdge, t, c, plotStep, plotLayout);
 
 //jQuery to update the plot as the value of the slider changes.
 
