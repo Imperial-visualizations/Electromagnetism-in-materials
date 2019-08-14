c69b38015d4d140a03b93bf249b476057495d1b3
Plankat <Plankat@users.noreply.github.com>
2019-08-14 15:15:35 +0100

    update

--- EM2/scripts/1PJRho.js
+++ EM2/scripts/1PJRho.js
@@ -1,19 +1,12 @@
-let cubeSize = 75;
+let cubeSize = 30;
 let prePos = [[0, 0, 0]];
 let cubeList = [];
+let cubeFace = [];
 let onValue = 1;
+let EMag = 100;
+let sN=1;
 
 //sliders input and display:
-let X_slider = document.getElementById("XRot");
-let X_slider_val = document.getElementById("XRot_Val");
-
-X_slider_val.innerHTML = X_slider.value;
-let XsliderVal = 0;
-
-X_slider.oninput = function () {
-    X_slider_val.innerHTML = this.value;
-    XsliderVal = this.value;
-};
 
 let Y_slider = document.getElementById("YRot");
 let Y_slider_val = document.getElementById("YRot_Val");
@@ -43,32 +36,87 @@ function setup() {
     frameRate(60);
 }
 
-class EFieldLine{
-    constructor(y=0,z=0,Rx=0,Ry=0,Rz=0) {
-        this.x=300;
-        this.y=y;
-        this.z=z;
-        this.Rx=Rx;
-        this.Ry=Ry;
-        this.Rz=Rz;
+function checkArray(a, b, c, list) {
+    let match = false;
+    let index = -100;
+    for (i = 0; i < list.length; i++) {
+        if (a == list[i][0]) {
+            if (b == list[i][1]) {
+                if (c == list[i][2]) {
+                    match = true;
+                    index = i;
+                    break;
+                    //console.log(i)
+                }
+            }
+        }
+    }
+    return [match, index];
+
+}
+
+class EFieldLine {
+    constructor(x = 0, y = 0, z = 0, Ry = 0, Rz = 0) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.Ry = Ry;
+        this.Rz = Rz;
     }
 
     DrawLine() {
         push();
         //strokeWeight(5);
         stroke('rgba(17, 161, 238, 0.45)');
-        rotateX(this.Rx);
-        rotateY(this.Ry);
-        rotateZ(this.Rz);
-        line(this.x,this.y,this.z,-this.x,this.y,this.z);
+        //rotateX(this.Rx);
+        //rotateY(this.Ry);
+        //rotateZ(this.Rz);
+        line(this.x, this.y, this.z, this.x + EMag * Math.sin(this.Ry) * Math.cos(this.Rz), this.y + EMag * Math.sin(this.Ry) * Math.sin(this.Rz), this.z + EMag * Math.cos(this.Ry));
         pop();
+        push();
+        noStroke();
+        fill('rgba(17, 161, 238, 0.7)');
+        translate(this.x + EMag * Math.sin(this.Ry) * Math.cos(this.Rz), this.y + EMag * Math.sin(this.Ry) * Math.sin(this.Rz), this.z + EMag * Math.cos(this.Ry));
+        //sphere(3);
+        //cone(10,10);
+
+        rotateZ(this.Rz);
+        rotateY(this.Ry);
+        rotateZ(-this.Rz);
+
+
+
+        beginShape();
+        vertex(5, 5, 0);
+        vertex(0, 0, 8);
+        vertex(5, -5, 0);
+        endShape();
+        beginShape();
+        vertex(5, -5, 0);
+        vertex(0, 0, 8);
+        vertex(-5, -5, 0);
+        endShape();
+        beginShape();
+        vertex(-5, -5, 0);
+        vertex(0, 0, 8);
+        vertex(-5, 5, 0);
+        endShape();
+        beginShape();
+        vertex(-5, 5, 0);
+        vertex(0, 0, 8);
+        vertex(5, 5, 0);
+        endShape();
+
+        pop()
     }
 }
 
-let linePos=[];
-for (i=-300;i<=300;i=i+100) {
-    for (n=-300;n<=300;n=n+100) {
-        linePos.push([i,n]);
+let linePos = [];
+for (let i = -200; i <= 200; i = i + 200) {
+    for (let n = -200; n <= 200; n = n + 200) {
+        for (let m = -200; m <= 200; m = m + 200) {
+            linePos.push([i, n, m]);
+        }
     }
 }
 
@@ -91,30 +139,102 @@ class element {
         box(cubeSize);
         pop();
     }
-}
 
-function checkArray([a, b, c]) {
-    let match = false;
-    for (i = 0; i < prePos.length; i++) {
-        if (prePos[i][0] == a) {
-            if (prePos[i][1] == b) {
-                if (prePos[i][2] == c) {
-                    match = true;
-                    break;
-                }
+    posFace() {
+        let faceList = [[this.x, this.y, this.z + (cubeSize / 2 + 1), "right"], [this.x, this.y, this.z - (cubeSize / 2 + 1), "left"], [this.x + (cubeSize / 2 + 1), this.y, this.z, "front"], [this.x - (cubeSize / 2 + 1), this.y, this.z, "back"], [this.x, this.y + (cubeSize / 2 + 1), this.z, "top"], [this.x, this.y - (cubeSize / 2 + 1), this.z, "bottom"]];
+
+        for (let i = 0; i < faceList.length; i++) {
+            let match = checkArray(faceList[i][0], faceList[i][1], faceList[i][2], cubeFace);
+            //console.log(match);
+            if (match[0] === true) {
+                cubeFace.splice(match[1], 1);
+            } else {
+                cubeFace.push(faceList[i]);
+                //console.log("111")
             }
         }
-    }
-    return match;
 
+    }
+
+    drawFace() {
+        for (let i = 0; i < cubeFace.length; i++) {
+            let p = 0;
+            push();
+            noStroke();
+            //fill("red");
+            //console.log(cubeFace[i]);
+            translate(cubeFace[i][0], cubeFace[i][1], cubeFace[i][2]);
+            if (cubeFace[i][3] === "right") {
+                p = EMag * Math.cos(YsliderVal * (Math.PI / 180));
+                rotateZ(0)
+            } else if (cubeFace[i][3] === "left") {
+                p = -EMag * Math.cos(YsliderVal * (Math.PI / 180));
+                rotateZ(0)
+            } else if (cubeFace[i][3] === "front") {
+                p = EMag * Math.sin(YsliderVal * (Math.PI / 180)) * Math.cos(ZsliderVal * (Math.PI / 180));
+                rotateY(Math.PI / 2)
+            } else if (cubeFace[i][3] === "back") {
+                p = -EMag * Math.sin(YsliderVal * (Math.PI / 180)) * Math.cos(ZsliderVal * (Math.PI / 180));
+                rotateY(Math.PI / 2)
+            } else if (cubeFace[i][3] === "top") {
+                p = EMag * Math.sin(YsliderVal * (Math.PI / 180)) * Math.sin(ZsliderVal * (Math.PI / 180));
+                rotateX(Math.PI / 2)
+            } else {
+                p = -EMag * Math.sin(YsliderVal * (Math.PI / 180)) * Math.sin(ZsliderVal * (Math.PI / 180));
+                rotateX(Math.PI / 2)
+            }
+            let str1 = 'rgba(255, 20, 20,';
+            let str2 = 'rgba(20, 20, 255,';
+            let str3 = String(Math.round(abs(p)) / 100) + ')';
+
+            if (0 >= p) {
+                fill(str1.concat(str3));
+            } else {
+                fill(str2.concat(str3));
+            }
+
+            plane(cubeSize - 2, cubeSize - 2);
+            pop();
+
+            if (sN==0) {
+                push();
+                stroke("black");
+                translate(cubeFace[i][0], cubeFace[i][1], cubeFace[i][2]);
+                if (cubeFace[i][3] === "right") {
+                    line(0, 0, 0, 0, 0, 10);
+                } else if (cubeFace[i][3] === "left") {
+                    line(0, 0, 0, 0, 0, -10);
+                } else if (cubeFace[i][3] === "front") {
+                    line(0, 0, 0, 10, 0, 0);
+                } else if (cubeFace[i][3] === "back") {
+                    line(0, 0, 0, -10, 0, 0);
+                } else if (cubeFace[i][3] === "top") {
+                    line(0, 0, 0, 0, 10, 0);
+                } else {
+                    line(0, 0, 0, 0, -10, 0);
+                }
+                pop()
+            }
+        }
+        //console.log(cubeFace)
+    }
+
+    //draw faces as another function. Each cube has 6 sides with a position of center. Position of center is in an array. When new cube is constructed, if new center cube side already exist in array, delete new and previous side (EG. set view=false), DONT DELETE CENTER POS
 }
 
+function showN() {
+    if (sN==1) {
+        sN = 0;
+    } else {
+        sN=1
+    }
+}
 function move_X() {
     let a = prePos[prePos.length - 1][0] - (cubeSize + 2);
     let b = prePos[prePos.length - 1][1];
     let c = prePos[prePos.length - 1][2];
-    let n = checkArray([a, b, c]);
-    if (n === false) {
+    let n = checkArray(a, b, c, prePos);
+    if (n[0] === false) {
         prePos.push([a, b, c]);
         onValue = 1;
     }
@@ -124,8 +244,8 @@ function move_Y() {
     let a = prePos[prePos.length - 1][1] + (cubeSize + 2);
     let b = prePos[prePos.length - 1][0];
     let c = prePos[prePos.length - 1][2];
-    let n = checkArray([b, a, c]);
-    if (n === false) {
+    let n = checkArray(b, a, c, prePos);
+    if (n[0] === false) {
         prePos.push([b, a, c]);
         onValue = 1;
     }
@@ -135,8 +255,8 @@ function movePlusX() {
     let a = prePos[prePos.length - 1][0] + (cubeSize + 2);
     let b = prePos[prePos.length - 1][1];
     let c = prePos[prePos.length - 1][2];
-    let n = checkArray([a, b, c]);
-    if (n === false) {
+    let n = checkArray(a, b, c, prePos);
+    if (n[0] === false) {
         prePos.push([a, b, c]);
         onValue = 1;
     }
@@ -146,8 +266,8 @@ function movePlusY() {
     let a = prePos[prePos.length - 1][1] - (cubeSize + 2);
     let b = prePos[prePos.length - 1][0];
     let c = prePos[prePos.length - 1][2];
-    let n = checkArray([b, a, c]);
-    if (n === false) {
+    let n = checkArray(b, a, c, prePos);
+    if (n[0] === false) {
         prePos.push([b, a, c]);
         onValue = 1;
     }
@@ -157,8 +277,8 @@ function move_Z() {
     let a = prePos[prePos.length - 1][2] - (cubeSize + 2);
     let b = prePos[prePos.length - 1][0];
     let c = prePos[prePos.length - 1][1];
-    let n = checkArray([b, c, a]);
-    if (n === false) {
+    let n = checkArray(b, c, a, prePos);
+    if (n[0] === false) {
         prePos.push([b, c, a]);
         onValue = 1;
     }
@@ -168,57 +288,66 @@ function movePlusZ() {
     let a = prePos[prePos.length - 1][2] + (cubeSize + 2);
     let b = prePos[prePos.length - 1][0];
     let c = prePos[prePos.length - 1][1];
-    let n = checkArray([b, c, a]);
-    if (n === false) {
+    let n = checkArray(b, c, a, prePos);
+    if (n[0] === false) {
         prePos.push([b, c, a]);
         onValue = 1;
     }
 }
 
 function deleteCube() {
-    if (cubeList.length > 1) {
-        cubeList.pop();
-        prePos.pop();
-    }
+    prePos = [[0, 0, 0]];
+    cubeList = [];
+    cubeFace = [];
+    let cube = new element();
+    cubeList.push(cube);
+    cube.posFace();
+    cube.drawElement();
+    cube.drawFace();
 }
 
+
 function draw() {
+
     clear();
     background("white");
     orbitControl();
     strokeWeight(3);
     push();
     stroke('rgba(0, 0, 0, 0.7)');
-    line(-300,0,0,300,0,0);
-    line(0,-300,0,0,300,0);
-    line(0,0,-300,0,0,300);
+    line(-300, 0, 0, 300, 0, 0);
+    line(0, -300, 0, 0, 300, 0);
+    line(0, 0, -300, 0, 0, 300);
     pop();
 
     push();
     stroke("green");
-    line(prePos[prePos.length-1][0], prePos[prePos.length-1][1], prePos[prePos.length-1][2],prePos[prePos.length-1][0]+100, prePos[prePos.length-1][1], prePos[prePos.length-1][2]);
+    line(prePos[prePos.length - 1][0], prePos[prePos.length - 1][1], prePos[prePos.length - 1][2], prePos[prePos.length - 1][0] + 50, prePos[prePos.length - 1][1], prePos[prePos.length - 1][2]);
     pop();
     push();
     stroke("red");
-    line(prePos[prePos.length-1][0], prePos[prePos.length-1][1], prePos[prePos.length-1][2],prePos[prePos.length-1][0], prePos[prePos.length-1][1]-100, prePos[prePos.length-1][2]);
+    line(prePos[prePos.length - 1][0], prePos[prePos.length - 1][1], prePos[prePos.length - 1][2], prePos[prePos.length - 1][0], prePos[prePos.length - 1][1] - 50, prePos[prePos.length - 1][2]);
     pop();
     push();
     stroke("blue");
-    line(prePos[prePos.length-1][0], prePos[prePos.length-1][1], prePos[prePos.length-1][2],prePos[prePos.length-1][0], prePos[prePos.length-1][1], prePos[prePos.length-1][2]+100);
+    line(prePos[prePos.length - 1][0], prePos[prePos.length - 1][1], prePos[prePos.length - 1][2], prePos[prePos.length - 1][0], prePos[prePos.length - 1][1], prePos[prePos.length - 1][2] + 50);
     pop();
 
     //console.log(prePos);
     if (onValue == 1) {
         let cube = new element(prePos[prePos.length - 1][0], prePos[prePos.length - 1][1], prePos[prePos.length - 1][2]);
         cubeList.push(cube);
+        cube.posFace();
     }
 
-    for (i=0;i<linePos.length;i++){
-        let line=new EFieldLine(linePos[i][0],linePos[i][1],XsliderVal*(Math.PI/180),YsliderVal*(Math.PI/180),ZsliderVal*(Math.PI/180));
+    for (let i = 0; i < cubeList.length; i++) {
+        cubeList[i].drawElement();
+        cubeList[i].drawFace();
+    }
+    for (let i = 0; i < linePos.length; i++) {
+        let line = new EFieldLine(linePos[i][0], linePos[i][1], linePos[i][2], YsliderVal * (Math.PI / 180), ZsliderVal * (Math.PI / 180));
         line.DrawLine();
     }
-    for (i = 0; i < cubeList.length; i++) {
-        cubeList[i].drawElement()
-    }
+    //console.log(cubeFace);
     onValue = 0;
 }
